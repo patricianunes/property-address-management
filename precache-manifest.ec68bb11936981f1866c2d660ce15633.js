c5eadf02ad1010478ed1dbe3df36b10a8f0d6678
Patricia Campos <patricia@krint.io>
2019-08-26 06:09:46 +1000

    Updates

---
+++ precache-manifest.ec68bb11936981f1866c2d660ce15633.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8d8897ba412b888668a1e0ff628144",
+    "url": "/patricianunes/property-address-management/index.html"
+  },
+  {
+    "revision": "ce42bea695e74593204b",
+    "url": "/patricianunes/property-address-management/static/js/2.1014466b.chunk.js"
+  },
+  {
+    "revision": "592eb3f000681f23264e",
+    "url": "/patricianunes/property-address-management/static/js/main.96c7404a.chunk.js"
+  },
+  {
+    "revision": "603fc99ccd7be1557c25",
+    "url": "/patricianunes/property-address-management/static/js/runtime~main.0ba46c6c.js"
+  }
+]);
