1b773fb39dd1e4b4248e77a1acb8f875aec88bcd
Patricia Campos <patricia@krint.io>
2019-08-26 06:05:42 +1000

    clear files from create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MaterialTable from "material-table";
 
 function App() {
+  const [state, setState] = React.useState({
+    columns: [
+      {
+        title: "Property Type",
+        field: "propertyType",
+        lookup: { 10: "House", 20: "Apartment" }
+      },
+      { title: "Unit number", field: "unitNumber", type: "numeric" },
+      { title: "Street number", field: "streetNumber", type: "numeric" },
+      { title: "Street", field: "street" },
+      { title: "Suburb", field: "suburb" },
+      { title: "Post Code", field: "postCode", type: "numeric" },
+      {
+        title: "State",
+        field: "state",
+        lookup: { 1: "NSW", 2: "VIC", 3: "QLD", 4: "SA", 5: "TAS", 6: "WA" }
+      }
+    ],
+    data: [
+      {
+        streetNumber: 36,
+        street: "Wyndham Street",
+        suburb: "Alexandria",
+        state: 1,
+        postCode: 2015
+      },
+      {
+        unitNumber: 2,
+        streetNumber: 18,
+        street: "Raglan Street",
+        suburb: "Alexandria",
+        state: 1,
+        postCode: 2016
+      }
+    ]
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialTable
+      title="Property Address Management"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          })
+      }}
+    />
   );
 }
 
